613270ee745fdd2a1b683d9864b273ac7014b870
Sameh Hassan <samehsmh@gmail.com>
2019-11-11 14:04:20 +0000

    home_service_robot: scripts: Point to our map file in test_navigation.sh
    Use the single_floor.yaml/pgm map file in test_navigation.sh.
    The TURTLEBOT_GAZEBO_WORLD_FILE env variable needs to be set as well.

--- home_service_robot/scripts/test_navigation.sh
+++ home_service_robot/scripts/test_navigation.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWD/src/home_service_robot/maps/single_floor.yaml" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch"
 
