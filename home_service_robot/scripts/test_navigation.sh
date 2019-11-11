78f53eacad8ed4851e47055c1a075679ee0d60f1
Sameh Hassan <samehsmh@gmail.com>
2019-11-11 14:11:35 +0000

    home_service_robot: scripts: test_navigation.sh: Set the Gazebo world file
    Set the TURTLEBOT_GAZEBO_WORLD_FILE to point to the single-floor world
    file.

--- home_service_robot/scripts/test_navigation.sh
+++ home_service_robot/scripts/test_navigation.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export TURTLEBOT_GAZEBO_WORLD_FILE="$PWD/src/home_service_robot/worlds/single-floor.world"
+
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWD/src/home_service_robot/maps/single_floor.yaml" &
