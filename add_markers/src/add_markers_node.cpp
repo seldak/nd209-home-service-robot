9e6bfdd000151bb3fbf6ff0ec09d11dc4777dfd4
Sameh Hassan <samehsmh@gmail.com>
2019-11-16 18:16:29 -0800

    add_markers: Add callback handlers for MarkerCommand messages
    According to the marker command, handle it using the handle_marker_command()
    callback, which here forwards to either handle_add_marker_command or
    handle_delete_marker_command. The pick_objects sends the appropriate command
    along with pose and orientation elements accordingly.

--- add_markers/src/add_markers_node.cpp
+++ add_markers/src/add_markers_node.cpp
@@ -1,18 +1,15 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <add_markers/MarkerCommand.h>
 
-int main( int argc, char** argv )
+static ros::Publisher marker_pub;
+
+// handle_drive_request: a callback function that executes whenever a drive_bot service is requested
+// This function publishes to the visualization_marker topic, handled by RViz
+void handle_add_marker_command(const add_markers::MarkerCommandPtr &cmd)
 {
-  ros::init(argc, argv, "add_markers");
-  ros::NodeHandle n;
-  ros::Rate r(1);
-  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-
-  // Set our initial shape type to be a cube
-  uint32_t shape = visualization_msgs::Marker::CUBE;
-
-  while( ros::ok() ) {
     visualization_msgs::Marker marker;
+
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
     marker.header.frame_id = "map";
     marker.header.stamp = ros::Time::now();
@@ -22,20 +19,20 @@ int main( int argc, char** argv )
     marker.ns = "add_markers";
     marker.id = 0;
 
-    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-    marker.type = shape;
+    // Set our Shape type to be a cube
+    marker.type = visualization_msgs::Marker::CUBE;
 
     // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
     marker.action = visualization_msgs::Marker::ADD;
 
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
-    marker.pose.position.y = 0;
+    marker.pose.position.x = cmd->pose_x;
+    marker.pose.position.y = cmd->pose_y;
     marker.pose.position.z = 0;
     marker.pose.orientation.x = 0.0;
     marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
+    marker.pose.orientation.z = cmd->orientation_z;
+    marker.pose.orientation.w = cmd->orientation_w;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
     marker.scale.x = 1.0;
@@ -43,24 +40,69 @@ int main( int argc, char** argv )
     marker.scale.z = 1.0;
 
     // Set the color -- be sure to set alpha to something non-zero!
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 1.0f;
-    marker.color.a = 1.0;
+    marker.color.r = 1.0f;
+    marker.color.g = 0.7f;
+    marker.color.b = 0.1f;
+    marker.color.a = 1.0f;
 
     marker.lifetime = ros::Duration();
 
     // Publish the marker
     while( marker_pub.getNumSubscribers() < 1 ) {
       if( !ros::ok() ) {
-        return 0;
+        return;
       }
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
     marker_pub.publish( marker );
+}
 
-    r.sleep();
-  }
+void handle_delete_marker_command(const add_markers::MarkerCommandPtr &cmd)
+{
+    visualization_msgs::Marker marker;
+
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "add_markers";
+    marker.id = 0;
+
+    // Set our Shape type to be a cube
+    marker.type = visualization_msgs::Marker::CUBE;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::DELETEALL;
+
+    marker_pub.publish( marker );
+}
+
+// handle_drive_request: a callback function that executes whenever a drive_bot service is requested
+// This function publishes to the visualization_marker topic, handled by RViz
+void handle_marker_command(const add_markers::MarkerCommandPtr &cmd)
+{
+    if( cmd->command == "add" ) {
+        handle_add_marker_command( cmd );
+    } else if( cmd->command == "delete" ) {
+        handle_delete_marker_command( cmd );
+    } else {
+        ROS_WARN("Invalid marker command %s", cmd->command.c_str() );
+    }
+}
+
+int main( int argc, char** argv )
+{
+  ros::init(argc, argv, "add_markers");
+  ros::NodeHandle n;
+
+  ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+  ros::Subscriber sub = n.subscribe("/add_markers/marker_command", 1, handle_marker_command);
+
+  ros::spin();
+
+  return 0;
 }
 
