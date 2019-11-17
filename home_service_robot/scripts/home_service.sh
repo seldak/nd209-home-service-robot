3f1dbe128a874dc62e610fb8b0a29a849407f3e8
Sameh Hassan <samehsmh@gmail.com>
2019-11-16 18:11:56 -0800

    home_service_robot: Add script to start whole project

---
+++ home_service_robot/scripts/home_service.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export TURTLEBOT_GAZEBO_WORLD_FILE="$PWD/src/home_service_robot/worlds/single-floor.world"
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWD/src/home_service_robot/maps/single_floor.yaml" &
+sleep 5
+xterm  -e  " rosrun add_markers add_markers_node" &
+sleep 5
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+xterm  -e  " rosrun pick_objects pick_objects_node"
+
