b0f34b13a133009699fa348b371353ab71ea3d78
Sameh Hassan <samehsmh@gmail.com>
2019-11-17 18:47:29 +0000

    home_service_robot: scripts: Change order of execution for add_markers

--- home_service_robot/scripts/home_service.sh
+++ home_service_robot/scripts/home_service.sh
@@ -5,9 +5,9 @@ xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$PWD/src/home_service_robot/maps/single_floor.yaml" &
 sleep 5
-xterm  -e  " rosrun add_markers add_markers_node" &
-sleep 5
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+xterm  -e  " rosrun add_markers add_markers_node" &
 sleep 5
 xterm  -e  " rosrun pick_objects pick_objects_node"
 
